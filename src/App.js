5f019a0d756c4536c421fa8ed68eb02dec06a100
PedroPudilio <paris4allus@gmail.com>
2024-12-18 04:12:00 +0300

    fix test's errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const greeting = "Это тестовый проект Селезнев Александр Антонович";
 
-  console.log('This is App ');
+
   return (
     <div className="App">
      <header className="App-header">
